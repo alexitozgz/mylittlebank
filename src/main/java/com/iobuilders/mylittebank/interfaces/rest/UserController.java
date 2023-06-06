f889f20bef568d455d464db8ceaf345249985438
alexitozgz <alejandro.ortariz.gracia@gmail.com>
2023-06-06 17:11:48 +0200

    Error registerUser improved

--- src/main/java/com/iobuilders/mylittebank/interfaces/rest/UserController.java
+++ src/main/java/com/iobuilders/mylittebank/interfaces/rest/UserController.java
@@ -2,6 +2,8 @@ package com.iobuilders.mylittebank.interfaces.rest;
 
 import com.iobuilders.mylittebank.domain.model.BankUser;
 import com.iobuilders.mylittebank.domain.services.UserService;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,9 +19,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping
-    public ResponseEntity<BankUser> registerUser(@RequestBody BankUser bankUserRequest) {
-        BankUser bankUser = userService.registerUser(bankUserRequest.getName(), bankUserRequest.getPhoneNumber(), bankUserRequest.getEmail());
-        return ResponseEntity.ok(bankUser);
+    @PostMapping("/registerUser")
+    public ResponseEntity<String> registerUser(@NotNull @RequestBody BankUser bankUserRequest) {
+        try {
+            userService.registerUser(bankUserRequest.getName(), bankUserRequest.getPhoneNumber(), bankUserRequest.getEmail());
+            return ResponseEntity.ok("Usuario registrado correctamente");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al registrar usuario");
+        }
     }
 }
